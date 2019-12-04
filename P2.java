8779a8b274c17afbcfa75a9d53906726aeed02a1
PSAIHARSHTIHA <58506061+PSAIHARSHTIHA@users.noreply.github.com>
2019-12-04 14:46:06 +0530

    Add files via upload

---
+++ P2.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class P2
+{
+public static void main(String arg[])
+{
+System.out.println("Welcome" +arg[0]);
+}
+}
+
---
+++ P3.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class P3
+{
+public static void main(String args[])
+{
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[1]); 
+System.out.println("sum of " +a+"and" +b+"is" +(a+b));
+}
+}
